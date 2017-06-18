4ebd25324aa071dd107a75005ff5b85773e9c388
Sebastien <miyoku157@gmail.com>
2017-06-18 10:45:47 +0200

    Add Input mobile System + Tap Double Tap System
    Lean touch plugin allow several kind of input
    Tap and Double tap implemented

---
+++ Vampiroid/Assets/Move.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean.Touch;
+
+public class Move : MonoBehaviour {
+
+
+    [Tooltip("The Speed of the player, *2 if double tap")]
+    public float Speed = 10;
+
+    [Tooltip("The distance from the finger the prefab will move")]
+    public float Distance = 10.0f;
+
+    private bool isRunning = false;
+
+    private Vector3 target;
+
+    private void Start()
+    {
+        target = transform.position;
+    }
+    private void Update()
+    {
+        if (!isRunning)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, Speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, Speed * 2 * Time.deltaTime);
+        }
+    }
+    public void MoveSimple(LeanFinger finger)
+    {
+        isRunning = false;
+        if(finger!= null)
+            target = finger.GetWorldPosition(Distance);
+    }
+
+    public void MoveDouble(LeanFinger finger)
+    {
+        isRunning = true;
+        if (finger != null)
+            target = finger.GetWorldPosition(Distance);
+    }
+}
+
