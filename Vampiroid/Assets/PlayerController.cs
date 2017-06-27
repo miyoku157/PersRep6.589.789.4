f7f2b334b8e172476ccb1a07bd3b6b900c990d4d
Exospector <pcheminel@gmail.com>
2017-06-27 19:46:12 +0200

    Camera focus && Basic stealth switch and state

---
+++ Vampiroid/Assets/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static System.Action OnStealthStateChange;
+
+    public GameObject playerObject;
+
+    bool stealthState;
+
+    void Awake()
+    {
+        OnStealthStateChange += UglyStealthDebug;
+
+        stealthState = true;
+    }
+
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.S))
+        {
+            SwitchStealthState();
+        }
+	}
+
+    void SwitchStealthState()
+    {
+        if(stealthState)
+            stealthState = false;
+        else
+            stealthState = true;
+
+        if(OnStealthStateChange != null)
+            OnStealthStateChange();
+    }
+
+    void UglyStealthDebug()
+    {
+        if (stealthState)
+            playerObject.GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+        else
+            playerObject.GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+    }
+}
